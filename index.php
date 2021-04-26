9fadbf812f79500b4dd391ecf1e3f94d5c7cf947
Dino Pacariz <dinopacariz@gmail.com>
2021-04-26 22:16:58 +0200

    error messages done

--- index.php
+++ index.php
@@ -2,38 +2,53 @@
 include_once("Products.php");
 
 $productsEncoded = json_encode($products, JSON_PRETTY_PRINT );
-/* echo $productsEncoded; */
+// echo $productsEncoded; 
 
 $response = [];
-
+$errors = [];
 // if sats som kollar ifall show eller category finns och om inte direkt skicka allt? Sparar tid?
 
+// Mahmouds api kollar kategori först och sen antal.
 
 $reqCount = $_GET['show'] ?? count($products);
 
-if ($reqCount <= 0 || $reqCount > count($products)) {
-    // must be a number between 1 and count($products)
-}
-
-
-foreach ($products as $key => $value) {
-    print_r($productsvalue);
-}
-
 if (isset($_GET['category'])) {
-    // $_GET['category'];
+    $queryCat = $_GET['category'];
+    $foundItems = [];
+
+    foreach ($products as $key => $product) {
+        if ($product['category'] == $queryCat) {
+            array_push($foundItems, $product);
+        }
+    }
+
+    if (!$foundItems) {
+        array_push($errors, array("Category" => "Category not found"));
+    }
 }
+
+if ($reqCount <= 0 || $reqCount > count($products) || !is_numeric($reqCount)) {
+    array_push($errors, array("Show" => "Show must be between 1 and 20"));
+}
+
+if ($errors) {
+    echo "hej";
+    
+    $errorsEncoded = json_encode($errors, JSON_PRETTY_PRINT );
+    print_r($errorsEncoded);
+}
+
 
 /* if (isset($_GET['show'])) {
 
 } */
+
 
 function UniqueRandomNumbersWithinRange($min, $max, $quantity) {
     $numbers = range($min, $max);
     shuffle($numbers);
     return array_slice($numbers, 0, $quantity);
 }
-
 
 /*
 Man ska kunna ange antal produkter via en GET-Request t.ex.
