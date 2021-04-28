3c3963300b08adbab2854d1e5546fc8bb0038f81
Dino Pacariz <dinopacariz@gmail.com>
2021-04-28 17:55:18 +0200

    ⚒ class construction in progress

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-include_once("Products.php");
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET");
@@ -7,56 +6,62 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
 header("Referrer-Policy: no-referrer");
 header("Content-Type: application/json; charset=UTF-8");
 
-$productsEncoded = json_encode($products, JSON_PRETTY_PRINT );
-// echo $productsEncoded; 
+class ProductList {
+    private static $queryCat;
+    private static $queryShow;
+    private static $products = [];
+    private static $foundItems = [];
 
-// todo fråga mahmoud om vi verkligen ska returnera json med JSON_PRETTY_PRINT, då försvinner ju syftet med JSON.
+    public static function main() {
+        include_once("Products.php");
+        self::$products = $products;
+        self::$queryCat = $_GET['category'] ?? null;
+        self::$queryShow = $_GET['show'] ?? null;
 
-$errors = [];
-$returnIndexes = [];
+        try {
+            self::$foundItems = self::$queryCat ? self::getCategory($products) : $products;
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
 
-$queryCat = $_GET['category'] ?? null;
-$foundItems = $queryCat ? getCategory($queryCat, $products) : $products;
-
-$queryShow = $_GET['show'] ?? null;
-$response = ($queryShow && $queryShow < count($foundItems)) ? selectItems() : $foundItems;
-
-
-function getCategory($queryCat, $products) {
-    foreach ($products as $key => $product) {
-        if ($product['category'] == $queryCat) {
-            array_push($foundItems, $product);
+        try {
+            $response = self::$queryShow ? self::selectItems() : self::$foundItems;
+            echo json_encode($response, JSON_UNESCAPED_UNICODE || JSON_PRETTY_PRINT);
+        } catch (Exception $e) {
+            echo $e->getMessage();
         }
     }
-    if (!$foundItems) {
-        array_push($errors, array("Category" => "Category not found"));
-    } else return $foundItems;
-}
+    public static function getCategory($products) {
+        $data = [];
 
-function selectItems() {
-    if ($queryShow <= 0 || $queryShow > count($products) || !is_numeric($queryShow)) {
-        array_push($errors, array("Show" => "Show must be a number between 1 and 20"));
-    } else {
-        $returnIndexes = UniqueRandomNumbersWithinRange(0, count($foundItems)-1, $queryShow);
+        foreach ($products as $key => $product) {
+            if ($product['category'] == self::$queryCat) {
+                array_push($data, $product);
+            }
+        }
+        if (!$data)
+            throw new Exception("Category not found");
+        return $data;
+    }
+    
+    public static function selectItems() {
+        if (self::$queryShow <= 0 || self::$queryShow > count(self::$products) || !is_numeric(self::$queryShow))
+            throw new Exception("Show must be a number between 1 and 20");
+        $returnIndexes = self::UniqueRandomNumbersWithinRange(0, count(self::$foundItems)-1, self::$queryShow);
         $data = [];
 
         foreach ($returnIndexes as $key => $index) {
-            array_push($data, $foundItems[$index]);
+            array_push($data, self::$foundItems[$index]);
         }
         return $data;
     }
+    public static function UniqueRandomNumbersWithinRange($min, $max, $quantity) {
+        $numbers = range($min, $max);
+        shuffle($numbers);
+        return array_slice($numbers, 0, $quantity);
+    }
 }
 
-if ($errors) {
-    echo json_encode($errors, JSON_UNESCAPED_UNICODE);
-} else {
-    echo json_encode($response, JSON_UNESCAPED_UNICODE);
-}
-
-function UniqueRandomNumbersWithinRange($min, $max, $quantity) {
-    $numbers = range($min, $max);
-    shuffle($numbers);
-    return array_slice($numbers, 0, $quantity);
-}
+ProductList::main();
 
 ?>
