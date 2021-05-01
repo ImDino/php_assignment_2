2bbaee7809b5894646d4d4791300433b0d9c3e31
Dino Pacariz <dinopacariz@gmail.com>
2021-05-01 15:19:18 +0200

    ✨ applied good practice changes

--- ProductList.php
+++ ProductList.php
@@ -8,16 +8,18 @@ class ProductList {
         
         $category = $_GET['category'] ?? null;
         $limit = $_GET['limit'] ?? null;
+        $foundItems = [];
+        $response = [];
         $errors = [];
+
         try {
-            $foundItems = [];
-            $foundItems = $category ? self::getCategory($category) : self::$allProducts;
+            $foundItems = $category ? self::get_category($category) : self::$allProducts;
         } catch (Exception $e) {
             array_push($errors, array("Category" => $e->getMessage()));
         }
         
         try {
-            $response = !is_null($limit) ? self::selectRandomItems($limit, $foundItems) : $foundItems;
+            $response = !is_null($limit) ? self::select_random_items($limit, $foundItems) : $foundItems;
         } catch (Exception $e) {
             array_push($errors, array("Show" => $e->getMessage()));
         }
@@ -27,7 +29,7 @@ class ProductList {
         } else echo json_encode($response, JSON_UNESCAPED_UNICODE);
     }
     
-    private static function getCategory($category) {
+    private static function get_category($category) {
         $data = [];
 
         foreach (self::$allProducts as $key => $product) {
@@ -40,11 +42,13 @@ class ProductList {
         return $data;
     }
     
-    private static function selectRandomItems($limit, $items) {
+    private static function select_random_items($limit, $items) {
         if ($limit <= 0 || $limit > count(self::$allProducts) || !is_numeric($limit))
             throw new Exception("Show must be a number between 1 and 20");
-        if (!$items) return;
-        $returnIndexes = self::UniqueRandomNumbersWithinRange(0, count($items)-1, $limit);
+        else if (!$items)
+            return null;
+        
+        $returnIndexes = self::unique_random_numbers_within_range(0, count($items)-1, $limit);
         $data = [];
 
         foreach ($returnIndexes as $key => $index) {
@@ -53,7 +57,7 @@ class ProductList {
         return $data;
     }
 
-    private static function UniqueRandomNumbersWithinRange($min, $max, $quantity) {
+    private static function unique_random_numbers_within_range($min, $max, $quantity) {
         $numbers = range($min, $max);
         shuffle($numbers);
         return array_slice($numbers, 0, $quantity);
